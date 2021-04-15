19b60971a7f578b4db885cff175a5681a6a86241
Leonardo Albuquerque <leeodesign@hotmail.com>
2021-04-14 22:38:39 -0300

    List books and delete

--- Books/BookController.js
+++ Books/BookController.js
@@ -3,7 +3,31 @@ const router = express.Router();
 const Book = require("./Book");
 
 router.get("/books", (request, response)=>{
-  response.send("teste");
+  Book.findAll().then((books)=>{
+  response.render("list", {books});
+  });
 });
+
+router.post("/book/save", (request, response)=>{
+  let title = request.body.title;
+  let author = request.body.author;
+  let isbn = request.body.isbn;
+
+  if(title && author && isbn){
+    Book.create({title, author, isbn});
+    response.redirect("/books");
+  }else{
+    response.redirect("/");
+  }
+});
+
+router.post("/book/delete", (request, response)=>{
+  let id = +request.body.id;
+  if(!isNaN(id) && id !== undefined){
+    Book.destroy({where: {id}}).then(()=>{
+      response.redirect("/books");
+    })
+  }
+})
 
 module.exports = router;
